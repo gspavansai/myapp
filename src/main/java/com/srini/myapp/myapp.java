210a0a5887ffaa548a54d37ce1e0ad2a8a70c06a
Srinivasa <Srinivasa.Garimella@elliemae.com>
2018-11-11 10:29:05 -0800

    First commit

---
+++ src/main/java/com/srini/myapp/myapp.java
@@ -0,0 +1,12 @@
+package com.srini.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(myapp.class, args);
+	}
+}
